5e56d25f17f3f5f5ce0b35b3b100fd85621e142e
Subha Rajagopal <73748056+Subha822@users.noreply.github.com>
2024-12-21 03:03:46 +0000

    Create Merge_Intervals.py

---
+++ LeetCode/Medium/Merge_Intervals.py
@@ -0,0 +1,33 @@
+# Brute Force Approach
+
+class Solution(object):
+    def merge(self, intervals):
+        n = len(intervals)
+        intervals.sort()
+        ans = []
+        for i in range(n):
+            start,end = intervals[i][0],intervals[i][1]
+            if ans and end <= ans[-1][1]:
+                continue
+            for j in range(i+1,n):
+                if intervals[j][0] <= end:
+                    end = max(end,intervals[j][1])
+                else:
+                    break
+            ans.append([start,end])
+        return ans
+        
+# Optimal Approach
+
+class Solution(object):
+    def merge(self, intervals):
+        n = len(intervals)
+        intervals.sort()
+        ans = []
+        for i in range(n):
+            if not ans or intervals[i][0] > ans[-1][1]:
+                ans.append(intervals[i])
+            else:
+                ans[-1][1] = max(ans[-1][1],intervals[i][1])
+        return ans
+        
--- README.md
+++ README.md
@@ -5,3 +5,4 @@ DAY  | ProblemSolution | LINK
 Day 01 | Set_Matrix_Zeroes | [Link](https://github.com/Subha822/Strivers_SDE_Sheet/blob/main/LeetCode/Medium/Set_Matrix_Zeroes.py)
 Day 02 | Best_Time_to_Buy_and_Sell_Stock | [Link](https://github.com/Subha822/Strivers_SDE_Sheet/blob/main/LeetCode/Easy/Best_Time_to_Buy_and_Sell_Stock.py)
 Day 03 | Rotate_Image | [Link](https://github.com/Subha822/Strivers_SDE_Sheet/blob/main/LeetCode/Medium/Rotate_Image.py)
+Day 04 | Merge_Intervals | [Link](https://github.com/Subha822/Strivers_SDE_Sheet/blob/main/LeetCode/Medium/Merge_Intervals.py)
