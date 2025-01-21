19e6547ee8a83f0e607cbadce4c7e6b0465645a1
Subha Rajagopal <73748056+Subha822@users.noreply.github.com>
2025-01-21 05:07:32 +0000

    Changes

--- LeetCode/Easy/Binary_Search.py
+++ LeetCode/Easy/Binary_Search.py
@@ -1,7 +1,6 @@
 class Solution(object):
     def search(self, nums, target):
         return self.binarySearch(nums, 0, len(nums) - 1, target)
-        
     def binarySearch(self, nums, low, high, target):
         if low > high:
             return -1
@@ -11,3 +10,18 @@ class Solution(object):
         elif target > nums[mid]:
             return self.binarySearch(nums, mid + 1, high, target)
         return self.binarySearch(nums, low, mid - 1, target)
+
+# Other Way
+class Solution(object):
+    def search(self, nums, target):
+        low = 0
+        high = len(nums) - 1
+        while low <= high:
+            mid = (low+high) // 2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                low = mid + 1
+            elif nums[mid] > target:
+                high = mid - 1
+        return -1
